19880dbbd956b213fd75a49d83deb72d9359e77f
titowoche30 <titowoche30@gmail.com>
2021-08-15 12:55:30 -0300

    fixing lambda args

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -87,7 +87,7 @@ def handler(event, context):
                     'HadoopJarStep': {
                         'Jar': 'command-runner.jar',
                         'Args': ['spark-submit',
-                                 '--class', 'myPackage.MyWriterObject'
+                                 '--class', 'myPackage.MyWriterObject',
                                  '--packages', 'io.delta:delta-core_2.12:1.0.0', 
                                  '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
                                  '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
@@ -103,7 +103,7 @@ def handler(event, context):
                     'HadoopJarStep': {
                         'Jar': 'command-runner.jar',
                         'Args': ['spark-submit',
-                                 '--class','myPackage.MyUpserterObject'
+                                 '--class','myPackage.MyUpserterObject',
                                  '--packages', 'io.delta:delta-core_2.12:1.0.0', 
                                  '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
                                  '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
